3dd49dd13e6b5c6e6b072233123e4b89b8e72801
lucasomar84 <lucasomar2003@gmail.com>
2022-11-18 15:58:58 -0300

    proyecto

---
+++ proyectoblanco/login.php
@@ -0,0 +1,58 @@
+<?php
+
+include('config.php');
+session_start();
+
+if (isset($_POST['login'])) {
+
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $query = $connection->prepare("SELECT * FROM users WHERE USERNAME=:username");
+    $query->bindParam("username", $username, PDO::PARAM_STR);
+    $query->execute();
+
+    $result = $query->fetch(PDO::FETCH_ASSOC);
+
+    if (!$result) {
+        echo '<p class="error">Username password combination is wrong!</p>';
+    } else {
+        if (password_verify($password, $result['password'])) {
+            $_SESSION['user_id'] = $result['id'];
+            echo '<p class="success">Congratulations, you are logged in!</p>';
+        } else {
+            echo '<p class="error">Username password combination is wrong!</p>';
+        }
+    }
+}
+
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>ingresar</title>
+    <link rel="stylesheet" href="css/style.css">
+
+  </head>
+  <body>
+
+    <form method="post" action="" name="signin-form">
+        <div class="form-element">
+            <label>Username</label>
+            <input type="text" name="username" pattern="[a-zA-Z0-9]+" required />
+        </div>
+        <div class="form-element">
+            <label>Password</label>
+            <input type="password" name="password" required />
+        </div>
+        <button type="submit" name="login" value="login">Log In</button>
+    </form>
+
+
+  </body>
+</html>
---
+++ proyectoblanco/sql/proyectoblanco.sql
@@ -0,0 +1,104 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 18-11-2022 a las 19:56:14
+-- Versión del servidor: 10.1.38-MariaDB
+-- Versión de PHP: 7.3.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `proyectoblanco`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `clients`
+--
+
+CREATE TABLE `clients` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `dni` varchar(20) DEFAULT NULL,
+  `consulta` varchar(200) DEFAULT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `clients`
+--
+
+INSERT INTO `clients` (`id`, `name`, `dni`, `consulta`, `created_at`) VALUES
+(1, 'Bill Gates', '123456789', 'Consulta', '2022-11-18 14:01:30'),
+(2, 'Elon Musk', '111222333', 'Oculista', '2022-11-18 14:01:30'),
+(3, 'Will Smith', '111333555', 'Oculista', '2022-11-18 14:01:30');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `username` varchar(25) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(100) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `email`) VALUES
+(1, 'a', '$2y$10$m5Z1WgDJwr/eUkAsEKttl.dOhbeXtW1V4G7BpzMq3H1AosHu9Omg2', 'a@gmail.com');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `clients`
+--
+ALTER TABLE `clients`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `clients`
+--
+ALTER TABLE `clients`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
