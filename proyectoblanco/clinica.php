a51b60bc7367361fc1f14447e226a989673f6ebf
lucas <lucas.boghossian.et@gmail.com>
2022-11-18 19:25:17 -0300

    subo

--- proyectoblanco/clinica.php
+++ proyectoblanco/clinica.php
@@ -25,22 +25,22 @@
               include('config.php');
               session_start();
 
-                $sql = SELECT * FROM clients;";
+                $sql = "SELECT * FROM clients;";
                 $result = $connection -> query($sql);
-               ?>
-                <?php while ($row = $result -> FETCH_ASSOC()){ ?>
+
+             while ($row = $result ->fetch(PDO::FETCH_ASSOC)){ ?>
                   <tr>
-                      <td><?php echo $row[id] ?>;</td>
-                      <td><?php echo $row[name] ?>;</td>
-                      <td><?php echo $row[dni] ?>;</td>
-                      <td><?php echo $row[consulta] ?>;</td>
-                      <td><?php echo $row[created_at] ?>;</td>
+                      <td><?php echo $row["id"] ?>;</td>
+                      <td><?php echo $row["name"] ?>;</td>
+                      <td><?php echo $row["dni"] ?>;</td>
+                      <td><?php echo $row["consulta"] ?>;</td>
+                      <td><?php echo $row["created_at"] ?>;</td>
                       <td>
                       <a class="btn btn-primary btn-sm"  href="/proyectoblanco/edit.php?id=">edit</a>
-                      <a  class="btn btn-primary btn-sm"  href="/proyectoblanco/delete.php?id=">edit</a>
+                      <a  class="btn btn-primary btn-sm"  href="/proyectoblanco/delete.php?id=">delete</a>
                       </td>
                   </tr>
-             <?  } ?>
+             <?php  } ?>
 
               </tbody>
 
