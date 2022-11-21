58cfe78ca59b7e6b8523702754954092dcb33cf5
Luca-Javier <luca.jav45@gmail.com>
2022-11-21 17:49:18 -0300

    df

--- blanco/student-create.php
+++ blanco/student-create.php
@@ -4,63 +4,66 @@ session_start();
 
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Student Create</title>
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+  <title>Student Create</title>
 </head>
+
 <body>
 
-    <div class="container mt-5">
+  <div class="container mt-5">
 
-        <?php include('message.php'); ?>
+    <?php include('message.php'); ?>
 
-        <div class="row">
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="card-header">
-                        <h4>Student Add
-                            <a href="index.php" class="btn btn-danger float-end">BACK</a>
-                        </h4>
-                    </div>
-                    <div class="card-body">
-                        <form action="code.php" method="POST">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="card">
+          <div class="card-header">
+            <h4>Student Add
+              <a href="index.php" class="btn btn-danger float-end">BACK</a>
+            </h4>
+          </div>
+          <div class="card-body">
+            <form action="code.php" method="POST" enctype="multipart/form-data">
 
-                            <div class="mb-3">
-                                <label>Student Name</label>
-                                <input type="text" name="name" class="form-control">
-                            </div>
-                            <div class="mb-3">
-                                <label>Student Email</label>
-                                <input type="email" name="email" class="form-control">
-                            </div>
-                            <div class="mb-3">
-                                <label>Student Phone</label>
-                                <input type="text" name="phone" class="form-control">
-                            </div>
-                            <div class="mb-3">
-                                <label>Student Course</label>
-                                <input type="text" name="course" class="form-control">
-                            </div>
-                             <div class="image-3">
-                              Añadir imagen: <input name="archivo" id="archivo" type="file"/>
-                            </div>
-                            <div class="mb-3">
-                                <button type="submit" name="save_student" class="btn btn-primary">Save Student</button>
-                            </div>
+              <div class="mb-3">
+                <label>Student Name</label>
+                <input type="text" name="name" class="form-control">
+              </div>
+              <div class="mb-3">
+                <label>Student Email</label>
+                <input type="email" name="email" class="form-control">
+              </div>
+              <div class="mb-3">
+                <label>Student Phone</label>
+                <input type="text" name="phone" class="form-control">
+              </div>
+              <div class="mb-3">
+                <label>Student Course</label>
+                <input type="text" name="course" class="form-control">
+              </div>
+              <div class="image-3">
+                Añadir imagen: <input name="archivo" id="archivo" type="file" />
+              </div>
+              <div class="mb-3">
+                <button type="submit" name="save_student" class="btn btn-primary">Save Student</button>
+              </div>
 
-                        </form>
-                    </div>
-                </div>
-            </div>
+            </form>
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
