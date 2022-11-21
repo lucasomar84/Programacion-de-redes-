24c200f4055ab3c8ab85a64b183856ae2eb88a6c
lucasomar84 <lucasomar2003@gmail.com>
2022-11-21 19:49:51 -0300

    subo imagen

--- blanco/index.php
+++ blanco/index.php
@@ -58,7 +58,7 @@ require 'dbcon.php';
                       <td><?= $student['email']; ?></td>
                       <td><?= $student['phone']; ?></td>
                       <td><?= $student['course']; ?></td>
-                      <td><img src="/imagenes-locales/<?= $student['imagen']; ?>"></td>
+                      <td><img src="./imagenes-locales/<?= $student['imagen']; ?>"></td>
                       <td>
                         <a href="student-view.php?id=<?= $student['id']; ?>" class="btn btn-info btn-sm">View</a>
                         <a href="student-edit.php?id=<?= $student['id']; ?>" class="btn btn-success btn-sm">Edit</a>
--- blanco/sql/blog.sql
+++ blanco/sql/blog.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 21-11-2022 a las 21:12:26
+-- Tiempo de generación: 21-11-2022 a las 23:43:48
 -- Versión del servidor: 10.1.38-MariaDB
 -- Versión de PHP: 7.3.2
 
@@ -29,12 +29,12 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `students` (
-  `id` int(6) NOT NULL,
+  `id` int(11) NOT NULL,
   `name` varchar(191) NOT NULL,
   `email` varchar(191) NOT NULL,
   `phone` varchar(191) NOT NULL,
   `course` varchar(191) NOT NULL,
-  `imagen` int(11) NOT NULL
+  `imagen` varchar(250) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -42,8 +42,7 @@ CREATE TABLE `students` (
 --
 
 INSERT INTO `students` (`id`, `name`, `email`, `phone`, `course`, `imagen`) VALUES
-(3, 'a', 'messi12@mail.com', '62562065', 'sexto', 0),
-(4, 'b', 'messi12@mail.com', '42432545', 'sexto', 0);
+(1, 'lucas omar', 'lucas123@gmail.com', '42432545', 'sexto', 'xd.jpg');
 
 -- --------------------------------------------------------
 
@@ -59,15 +58,6 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Volcado de datos para la tabla `users`
---
-
-INSERT INTO `users` (`id`, `username`, `password`, `email`) VALUES
-(1, 'a', '$2y$10$m5Z1WgDJwr/eUkAsEKttl.dOhbeXtW1V4G7BpzMq3H1AosHu9Omg2', 'a@gmail.com'),
-(2, 'lucas', '$2y$10$TLsH8tbcdnYj7/BPMZfLfu1LLL1PAiL4L5lc/y9uySg0SFgKahEC.', 'lucasacuna87@gmail.com'),
-(3, 'b', '$2y$10$dA2v4pRa1iglg0VdWWdPy.WLQUgjet313mdRfvvhK8cfn2sRBQkjm', 'b@gmail.com');
-
---
 -- Índices para tablas volcadas
 --
 
@@ -78,13 +68,6 @@ ALTER TABLE `students`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indices de la tabla `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `username` (`username`);
-
---
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -92,13 +75,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de la tabla `students`
 --
 ALTER TABLE `students`
-  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT de la tabla `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
